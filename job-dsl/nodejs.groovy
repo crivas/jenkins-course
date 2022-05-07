338a4c6e927e9820a0a14cd457713c0dcbe826df
Carlos Rivas <carlos.rivas82@gmail.com>
2022-05-06 21:15:30 -0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,7 +1,7 @@
 job('NodeJS example') {
     scm {
         git('git@github.com:crivas/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
+            node / gitConfigName('crivas')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
